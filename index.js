4f0397945f936648de54afa757c2ef7a4556bc48
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-03-22 21:29:58 -0600

    Use promises instead of callbacks

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-const Storage = require('@google-cloud/storage').Storage;
+const { Storage } = require('@google-cloud/storage');
 const BaseAdapter = require('ghost-storage-base');
 
 class GoogleCloudStorage extends BaseAdapter {
   constructor(config) {
     super();
+
     this.config = config;
     this.storage = new Storage({
       projectId: config.projectId,
       keyFilename: config.keyFilename
     });
+
     this.bucket = this.storage.bucket(config.bucketName);
   }
-  
-  save(file, targetDir) {
+
+  save(file) {
     return new Promise((resolve, reject) => {
       const options = {
-        destination: `${targetDir}/${file.name}`,
+        destination: file.name,
         resumable: false,
         public: true,
       };
-      
-      this.bucket.upload(file.path, options, (err, file) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(file.publicUrl());
-      });
+
+      this.bucket.upload(file.path, options)
+        .then(data => {
+          resolve(data.shift().publicUrl());
+        })
+        .catch(err => reject(err));
     });
   }
   
-  exists(fileName, targetDir) {
-    return this.bucket.file(`${targetDir}/${fileName}`).exists();
+  exists(fileName) {
+    return new Promise((resolve, reject) => {
+      this.bucket.file(fileName).exists()
+        .then(exists => resolve(exists))
+        .catch(err => reject(err));
+    });
   }
   
   serve() {
     return (req, res, next) => next();
   }
   
-  delete(fileName, targetDir) {
-    return this.bucket.file(`${targetDir}/${fileName}`).delete();
+  delete(fileName) {
+    return new Promise((resolve, reject) => {
+      this.bucket.file(fileName).delete()
+        .then(() => resolve())
+        .catch(err => reject(err));
+    });
   }
   
   read(options) {
